aa3bbe0eeaa45e67f17f0f1bc741d29b06c7b62e
satish023 <83802083+satish023@users.noreply.github.com>
2024-01-02 09:11:08 +0530

    Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+   
+   <>
+      {/* // Starting with the new project called chatGPT-2.0 using React JS, Vite & Tailwind CSS. We are here learning concepts of 
+    // React JS & having some hands on Vite & Tailwind CSS.
+    
+
+    //1 main div and header body and input 3 div */}
+    
+      <div className='bg-[#f95959] h-screen'>
+     
+      </div></>
+  )
+}
+
+export default App

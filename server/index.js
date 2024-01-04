0a350811dee6a1b5de2b7d45e239e3124912e373
satish023 <83802083+satish023@users.noreply.github.com>
2024-01-04 09:20:24 +0530

    server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import env  from 'dotenv';
+import  {Configuration, OpenAIApi} from 'openai'
+
+
+const app = express()
+
+env.config()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.listen("3080", ()=> console.log("Running on port 3080"))
+
+app.get("/", (req, res) => {
+    res.send("Responding")
+})

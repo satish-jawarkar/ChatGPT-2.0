e0727436fc84339fff76d31f733cf11968c3101d
satish023 <83802083+satish023@users.noreply.github.com>
2024-01-05 19:50:15 +0530

    API

--- server/index.js
+++ server/index.js
@@ -12,6 +12,13 @@ env.config()
 app.use(cors())
 app.use(bodyParser.json())
 
+const configuration = new Configuration({
+    organization: "org-Oq8C8hKQFiOwrQJM4UpFWGLW",
+    apiKey: process.env.APIKEY 
+})
+
+const openai = new OpenAIApi(configuration)
+
 app.listen("3080", ()=> console.log("Running on port 3080"))
 
 app.get("/", (req, res) => {
